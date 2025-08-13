344f3531cdfa14f66039650cda7abe4e6c2036f3
TrevorJMoore <moore3112000@hotmail.com>
2025-08-12 23:41:07 -0700

    Create initial structure for Sensor and SensorReading.

---
+++ simulator/src/main/java/com/rles/simulator/sensors/SensorReading.java
@@ -0,0 +1,35 @@
+package com.rles.simulator.sensors;
+
+enum ReadingStatus {
+	ON,
+	OFF,
+	STALE,
+	WARN,
+	FAULT
+}
+
+// Represents a reading from a sensor
+public class SensorReading{
+
+	private final int sensorId;
+	private final long timestamp;
+	private final int sequence;
+	private final double value;
+	private ReadingStatus readingStatus;
+	
+	public SensorReading(int sensorId, long timestamp, int sequence, double value, ReadingStatus readingStatus) {
+		this.sensorId = sensorId;
+		this.timestamp = timestamp;
+		this.sequence = sequence;
+		this.value = value;
+		this.readingStatus = readingStatus;
+	}
+	
+	// Accessor Methods
+	public int getSensorId() { return sensorId;	}
+	public long getTimestamp() { return timestamp; }
+	public int getSequence() { return sequence; }
+	public double getValue() { return value; }
+	public ReadingStatus getReadingStatus() { return readingStatus; }
+	
+}
