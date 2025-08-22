8934da4a779f9eefdda523272fe4f5c95f0d9b66
TrevorJMoore <moore3112000@hotmail.com>
2025-08-21 22:50:45 -0700

    Introduce the SimulatorContext.

---
+++ simulator/src/main/java/com/rles/simulator/SimulatorContext.java
@@ -0,0 +1,9 @@
+package com.rles.simulator;
+
+/*	SimulatorContext - Stores sensor readings, provides time/units, and lets any sensors derived of other sensor readings read those readings.
+ * 
+ * 
+ */
+public class SimulatorContext {
+
+}
